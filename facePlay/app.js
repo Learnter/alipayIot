a71c28b34a2d935bc171c3eb1f2d2956b412aa96
黄椿任 <17818556509@163.com>
2020-03-05 11:08:33 +0800

    websocket 优化

--- facePlay/app.js
+++ facePlay/app.js
@@ -12,13 +12,13 @@ App({
   }, 
   onLaunch(options) {
     let that = this;
-    //设置机器设备的SN进入缓存
-    // let { data } = my.getStorageSync({ key: 'equipment_SN' });
-    // if (!data) {
-    //   let { value } = my.ix.getSysPropSync({ key: 'ro.serialno' });
-    //   my.setStorageSync({ key: 'equipment_SN', data: value });
-    // }
-    //设置机器设备的SN进入缓存
+    // 设置机器设备的SN进入缓存
+    let { data } = my.getStorageSync({ key: 'equipment_SN' });
+    if (!data) {
+      let { value } = my.ix.getSysPropSync({ key: 'ro.serialno' });
+      my.setStorageSync({ key: 'equipment_SN', data: value });
+    }
+    // 设置机器设备的SN进入缓存
 
     that.linkSocket();
   },
@@ -29,9 +29,8 @@ App({
     my.connectSocket({
       url: that.globalData.socketUrl,
       success() {
-        // let { value } = my.ix.getSysPropSync({ key: 'ro.serialno' });
-        // that.clientData = {sn:value,type:1};
-        that.globalData.clientData = "111";
+        let { value } = my.ix.getSysPropSync({ key: 'ro.serialno' });
+        that.globalData.clientData = {sn:value,type:1};
         that.initEventHandle();
     	  that.globalData.firstSend = true;
       }
@@ -41,21 +40,17 @@ App({
   //绑定事件
   initEventHandle() {
     var that = this
-    that.globalData.limit = 0;
-
+    // console.log(that.globalData.firstSend);
    if(that.globalData.firstSend === false) {
     my.onSocketOpen(() => {
-    console.log('链接一打开');
+    // console.log('链接一打开');
       that.intervalObj = setInterval(function(){
         that.timedSend();
-      },10000)  
+      },30000)  
     })
 
    my.onSocketMessage(function(res) {
      var that = this;
-     // that.globalData.intervalObj = setTimeout(function(){
-     //    that.timedSend();
-     //  },30000)  
         let serverData = JSON.parse(res.data)
         if (serverData && serverData.type == 1) {
           $http.POST($api.swiper.amountPay, { price: serverData.price }).then((res) => {
@@ -67,7 +62,7 @@ App({
                 success: (r) => {
                   $http.POST($api.orderPay.alipy, { 'bar_code': r.barCode, 'order_sn': r.bizNo }).then((res) => { //提交刷脸支付二维码及订单单号
                     if (res.data.code === 200) {
-                      my.showToast({ content: '支付成功'});
+                      my.showToast({content: '支付成功'});
                     } else {
                       my.showToast({
                         type: 'exception',
@@ -97,12 +92,12 @@ App({
         }
       })
     my.onSocketError((res) => {
-      console.log('WebSocket连接打开失败')
+      // console.log('WebSocket连接打开失败')
       this.reconnect();
     })
 
    my.onSocketClose((res) => {
-      console.log('WebSocket 已关闭！')
+      // console.log('WebSocket 已关闭！')
         this.reconnect();
     })
    }
@@ -110,27 +105,25 @@ App({
 
   timedSend: function(){ //定时发送信息保持心跳
     let that = this;
-    
     my.sendSocketMessage({
       data: that.globalData.clientData, // 需要发送的内容
       success:function(){
-       
-        console.log("发送消息成功：",that.globalData.clientData);
+        // console.log("发送消息成功：",that.globalData.clientData);
         that.globalData.clientData = "pong";
       }
     });
   },
 
   reconnect:function(){ //网络错误重连
-      console.log(this.timer);
-    // return this.linkSocket();
     var that = this; 
     clearInterval(that.intervalObj);
     clearTimeout(that.timer);
-      that.timer = setTimeout(() => {
-      console.log("socket重连",that.globalData.limit);
-      that.globalData.limit++;
+    if(that.globalData.limit < 10){
+       that.timer = setTimeout(() => {
+      //  console.log("socket重连",that.globalData.limit);
+        that.globalData.limit++;
         that.linkSocket();
       }, 30000);
+    }
   }
 });
